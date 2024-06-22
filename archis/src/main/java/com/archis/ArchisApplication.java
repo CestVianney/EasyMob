657441039679f35f7a5e6ce6431ec78fcf29b1d0
CestVianney <vianneycharpentier@gmail.com>
2024-06-22 13:24:58 +0200

    1st push

---
+++ archis/src/main/java/com/archis/ArchisApplication.java
@@ -0,0 +1,13 @@
+package com.archis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchisApplication.class, args);
+	}
+
+}
---
+++ archis/src/test/java/com/archis/ArchisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
